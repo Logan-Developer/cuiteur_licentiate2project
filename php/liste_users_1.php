d01386779bbde5105d7949be4f94a74c122f82ab
Logan-Developer <logan2.humbert@gmail.com>
2022-03-25 15:25:43 +0100

    liste_users_1: Basic page

--- php/liste_users_1.php
+++ php/liste_users_1.php
@@ -5,3 +5,12 @@
  * 
  * Script to list all users
  */
+
+ include_once './php/bibli_generale.php';
+ include_once './php/bibli_cuiteur.php';
+
+ hl_aff_debut('Liste des utilisateurs');
+
+ echo '<h1>Liste des utilisateurs</h1>';
+
+ hl_aff_fin();
